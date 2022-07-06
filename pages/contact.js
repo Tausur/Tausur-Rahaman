38a2784c2d1fd81a448a91fb30bfcdd273efe782
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-07-06 12:31:24 +0600

    Add files via upload

--- pages/contact.js
+++ pages/contact.js
@@ -38,29 +38,29 @@ const contact = () => {
         <form onSubmit={handleSubmit} className="bg-gray-900 md:w-96 md:h-[520px] rounded-3xl" style={{
           "boxShadow": "25px 25px 75px rgba(0,0,0,0.25), 10px 10px 70px rgba(0,0,0,0.25), inset 5px 5px 10px rgba(0,0,0,0.5), inset 5px 5px 10px rgba(255,255,255,0.2), inset -5px -5px 15px rgba(0,0,0,0.75)"
         }}>
-          <h1 className='md:text-3xl font-semibold text-gray-300 flex justify-center items-center pt-5 py-7'>Get in touch</h1>
+          <h1 className='md:text-3xl font-semibold text-gray-300 flex justify-center items-center pt-5 py-7 text-3xl'>Get in touch</h1>
           <div className='my-4'>
-            <label className='md:text-xl justify-center items-center md:mx-7 my-2'>Name</label>
+            <label className='md:text-xl justify-center items-center md:mx-7 my-2 font-semibold mx-4'>Name</label>
             <div className='flex justify-center items-center'>
               <BsFillPersonFill className='text-2xl md:ml-7 md:mx-1 flex justify-center items-center' />
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='focus:outline-none rounded-xl md:mx-2 text-2xl border-b-2 text-black h-10' />
+              <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='focus:outline-none rounded-full md:mx-2 text-xl border-b-2 text-black h-10' />
             </div>
           </div>
           <div className='my-4'>
-            <label className='md:text-xl justify-center items-center md:mx-7 my-2'>Email</label>
+            <label className='md:text-xl justify-center items-center md:mx-7 my-2 font-semibold mx-4'>Email</label>
             <div className='flex justify-center items-center'>
               <MdEmail className='text-3xl md:ml-7 md:mx-1 flex justify-center items-center' />
-              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className='focus:outline-none rounded-xl md:mx-2 text-2xl border-b-2 text-black h-10' />
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className='focus:outline-none rounded-full md:mx-2 text-xl border-b-2 text-black h-10' />
             </div>
           </div>
           <div>
-            <label className='md:text-xl justify-center items-center md:mx-7 '>Message</label>
+            <label className='md:text-xl justify-center items-center md:mx-7 font-semibold mx-4'>Message</label>
             <div className='flex justify-center items-center my-3'>
-              <MdMessage className='text-2xl md:ml-7 md:mx-1 flex justify-center items-center' />
-              <textarea type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='focus:outline-none rounded-xl md:mx-2 text-xl border-b-2 text-black' cols={27} rows={4} />
+              <MdMessage className='text-2xl md:ml-7 md:mx-1 flex justify-center items-center ml-4' />
+              <textarea type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='focus:outline-none rounded-xl md:mx-2 text-xl border-b-2 text-black mr-4' cols={27} rows={4} />
             </div>
           </div>
-          <button className='rounded-2xl text-xl bg-blue-400 py-2 px-5 flex justify-center items-center m-auto my-3 text-white'>
+          <button className='rounded-2xl text-xl bg-blue-500 py-2 px-5 flex justify-center items-center m-auto my-3 text-white shadow-2xl'>
             Send
           </button>
         </form>
