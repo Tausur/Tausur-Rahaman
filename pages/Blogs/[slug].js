ef4a5090b03d109521003e6f9f4e381a09c5b079
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-07-08 11:50:32 +0600

    Add files via upload

--- pages/Blogs/[slug].js
+++ pages/Blogs/[slug].js
@@ -23,12 +23,15 @@ const Post = ({ blogs, comments }) => {
                 headers: {
                     'Content-Type': "application/json"
                 }
-            })
+            }).then(res => { router.push(`/Blogs/${slug}`) })
         } catch (error) {
             console.log(error)
         }
 
     }
+
+    let allCom = []
+    let my;
 
     return (
         <div>
@@ -43,7 +46,7 @@ const Post = ({ blogs, comments }) => {
                             <p className='md:text-xl font-semibold md:px-32 md:pb-20  text-lg m-5'>{blog.desc}</p>
                             <p className='px-32 pb-20 text-3xl'>Comments</p>
                             <form className='justify-center items-center md:justify-start md:items-start mb-20 mx-7' onSubmit={handler}>
-                                <input type="text" placeholder='Your name' value={name} onChange={(e)=>setName(e.target.value)} className='focus:outline-none border-black md:text-lg border-b md:w-1/4 w-3/4 md:ml-32 md:mb-12 mb-6 block'/>
+                                <input type="text" placeholder='Your name' value={name} onChange={(e) => setName(e.target.value)} className='focus:outline-none border-black md:text-lg border-b md:w-1/4 w-3/4 md:ml-32 md:mb-12 mb-6 block' />
                                 <input onChange={(e) => setBody(e.target.value)} value={body} type="text" className='focus:outline-none border-black md:text-lg border-b md:w-1/2 w-3/4 md:ml-32 md:mb-12' placeholder='Post a comment' />
                                 <button type="submit" onClick={(e) => setPost(blog.slug)}>
                                     <IoIosSend className='text-3xl text-blue-500' />
@@ -53,26 +56,31 @@ const Post = ({ blogs, comments }) => {
                     </div>
                 }
             })}
-            <div>
+            <div className='pb-10'>
                 {comments.map((comment) => {
                     if (comment.post == slug) {
-                        return <div key={comment._id}>
-                            <div className='w-3/4 h-full bg-gray-300 md:ml-32 rounded-xl md:mb-8 my-5 mx-5'>
-                                <div className='flex  items-center'>
-                                    <div className='bg-blue-500 text-lg rounded-full w-10 h-10 flex justify-center items-center text-white md:ml-8 md:my-8 mx-2 my-2'>
-                                        {comment.name[0]}
-                                    </div>
-                                    <div className='text-xl font-semibold pl-2'>
-                                        {comment.name}
-                                    </div>
-                                </div>
-                                <div className='text-xl font-semibold md:pl-20 md:pb-8 mx-2 my-2 pb-2'>
-                                    {comment.body}
-                                </div>
-                            </div>
-                        </div>
+                        allCom.push(comment)
+                        my = allCom.reverse()
                     }
                 })}
+                <div>
+                   {my.map((c)=>{
+                    return <div className='w-3/4 h-full bg-gray-300 md:ml-32 rounded-xl md:mb-2 my-2 mx-2'>
+                    <div className='flex  items-center'>
+                        <div className='bg-blue-500 text-lg rounded-full w-10 h-10 flex justify-center items-center text-white md:ml-8 md:my-2 mx-1 my-1 shadow-xl'>
+                            {c.name[0]}
+                        </div>
+                        <div className='text-xl font-semibold pl-2'>
+                            {c.name}
+                        </div>
+                    </div>
+                    <div className='text-xl font-semibold md:pl-20 md:pb-3 mx-2 my-1 pb-1'>
+                        {c.body}
+                    </div>
+                </div>
+                   })
+                   }
+                </div>
             </div>
         </div >
     )
