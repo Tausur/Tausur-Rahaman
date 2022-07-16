9ace181cf350858f3b7f8b5871b2ee4fc53d6e9d
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-07-16 14:48:19 +0600

    Update adminLogin.js

--- pages/api/adminLogin.js
+++ pages/api/adminLogin.js
@@ -8,7 +8,7 @@ const handler = (req,res)=> {
       res.setHeader(
         "Set-Cookie",
         cookie.serialize("token", process.env.TOKEN, {
-          maxAge: 2 * 2,
+          maxAge: 2,
           sameSite: "strict",
           path: "/",
         })
