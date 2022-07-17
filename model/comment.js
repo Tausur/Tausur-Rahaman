a0a57eb2e9d4180aaf83e3cfff5f8d70f13d2479
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-07-17 12:05:00 +0600

    Delete comment.js

--- model/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const CommentSchema = new mongoose.Schema({
-    body : {require : true , type : String},
-    post : {type : String , require : true},
-    name : {type : String , require : true}
-},{timestamps: true})
-
-export default mongoose.models.aComment ||mongoose.model("aComment",CommentSchema);
