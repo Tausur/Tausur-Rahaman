5977c4a28499c1290f12b81d91c993ce64fe7dcc
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-07-18 12:42:54 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import '../styles/globals.css'
-import Head from 'next/head'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return <>
-    <Head>
-      <title>Tausur Rahaman</title>
-    </Head>
     <div className='fixed'>
       <Navbar />
     </div>
